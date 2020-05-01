333246cde96bab2d20585b50cf3c86fb1ba0c2a2
Eka <obyprof@gmail.ccom>
2020-05-02 02:05:20 +0700

    fm

---
+++ app/src/main/java/com/apps/latihan3dbroom/AppDatabase.java
@@ -0,0 +1,10 @@
+package com.apps.latihan3dbroom;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {AktivisEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract AktivisDao aktivisDao();
+}
